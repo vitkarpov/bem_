77189567e664427c113da152080f8ffab45d3953
Alexey Ten (Lynn) <alexeyten@gmail.com>
2016-07-18 11:21:28 +0300

    Поддержка объекта первым параметром

--- tests/spec/modifiers.js
+++ tests/spec/modifiers.js
@@ -15,9 +15,6 @@ describe('Modifiers -> ', function() {
     });
     ([
         { chain: [
-            { method: 'm', args: ['mod'] }
-        ], expected: 'block block_mod' },
-        { chain: [
             { method: 'm', args: ['mod', true] }
         ], expected: 'block block_mod' },
         { chain: [
@@ -49,5 +46,50 @@ describe('Modifiers -> ', function() {
             });
             expect(String(this.b)).to.be.equal(test.expected);
         });
+    });
+});
+
+describe('Modifiers (Object) -> ', function() {
+    beforeEach(function() {
+        this.b = new BEMClassName('block');
+    });
+    it('m({}) => "block"', function() {
+        expect(String(this.b.m({}))).to.be.equal('block');
+    });
+    it('m({"mod":undefined}) => "block"', function() {
+        expect(String(this.b.m({mod: undefined}))).to.be.equal('block');
+    });
+    ([
+        { chain: [
+            { method: 'm', args: [{mod: true}] }
+        ], expected: 'block block_mod' },
+        { chain: [
+            { method: 'm', args: [{mod: false}] }
+        ], expected: 'block' },
+        { chain: [
+            { method: 'm', args: [{m1: true, m2: false, m3: 42}] }
+        ], expected: 'block block_m1 block_m3_42' },
+    ]).forEach((test) => {
+        it(methodsChainToString(test.chain) + ' => "' + test.expected + '"', function() {
+            test.chain.forEach((item) => {
+                this.b[item.method].apply(this.b, item.args);
+            });
+            expect(String(this.b)).to.be.equal(test.expected);
+        });
+    });
+});
+
+describe('Modifiers (Bad args) -> ', function() {
+    beforeEach(function() {
+        this.b = new BEMClassName('block');
+    });
+    it('Array should be ignored', function() {
+        expect(String(this.b.m([1,2,3]))).to.be.equal('block');
+    });
+    it('null should be ignored', function() {
+        expect(String(this.b.m(null))).to.be.equal('block');
+    });
+    it('number should be ignored', function() {
+        expect(String(this.b.m(13))).to.be.equal('block');
     });
 });
